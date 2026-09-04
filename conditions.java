a9e8ea4d41e7edaea0a76df9a451238bb3021af1
Md. Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-04 15:43:20 +0600

    Implement comparison of two integers in loop class

---
+++ conditions.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+//public class loop {
+//    public static void main(String args[]){
+//        Scanner sc = new Scanner(System.in);
+//        int age = sc.nextInt();
+//        if(age>18)System.out.println("Adult");
+//        else System.out.println("Child");
+//    }
+//}
+
+//public class loop {
+//    public static void main(String args[]){
+//        Scanner inp = new Scanner(System.in);
+//        int x=inp.nextInt();
+//        if(x%2==0)System.out.println("EVEN");
+//        else System.out.println("ODD");
+//    }
+//}
+
+public class loop{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("X = ");
+        int x=sc.nextInt();
+        System.out.print("Y = ");
+        int y=sc.nextInt();
+        if(x==y)System.out.println("EQUAL");
+        else if(x<y)System.out.println("Y BIGGER");
+        else System.out.println("X BIGGER");
+    }
+}
