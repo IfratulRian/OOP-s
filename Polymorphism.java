510f57d23bed5c5d1375573ac5e02268a644c635
Ifratul Islam Rian <ifrat.rian0007@gmail.com>
2026-09-08 21:51:26 +0600

    No. of Seq.

--- Polymorphism.java
+++ Polymorphism.java
@@ -6,10 +6,19 @@ public class Polymorphism {
     void show(int n){
         System.out.println("The number is "+ n);
     }
+    // No. of sequence
+    void show(int n, String s){
+        System.out.println("The number is "+n+" & the string is "+s);
+    }
+    void show(String s, int n){
+        System.out.println("The string is "+s+" & the number is "+n);
+    }
 
     public static void main(String args[]){
         Polymorphism x = new Polymorphism();
         x.show();
         x.show(10);
+        x.show(10,"Hello World");
+        x.show("Hello World",10);
     }
 }
